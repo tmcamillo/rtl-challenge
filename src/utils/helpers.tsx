d03b2fce6464bcd232d386a97a0ded7d603da697
tmcamillo <tmcamillo@users.noreply.github.com>
2022-11-14 20:43:33 +0000

    deploy: 1a0d6f547fa2db8461a5f31a05cc32965e6dda32

--- src/utils/helpers.tsx
+++
@@ -1,5 +0,0 @@
-import { ICrop } from './data/bundle-interface';
-
-export function getImageType(array: ICrop[], imageType: string) {
-  return array.find((item: ICrop) => item.type === imageType)?.path || '';
-}
